f05889bc242d78d1fccb3b0a2a50df1e98627408
Karsten Gohm <karsten.gohm@internations.org>
2023-02-12 20:21:40 +0100

    WIP

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  ) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ src/components/Footer.tsx
@@ -0,0 +1,40 @@
+const Footer = () => (
+  <div className="bg-gray-100 pt-2 ">
+    <div
+      className="flex pb-5 px-3 m-auto pt-5 text-gray-800 text-sm flex-col
+      md:flex-row max-w-6xl"
+    >
+      <div className="mt-2">
+        © Copyright 2020. All Rights Reserved.
+        <br />
+        Bungie Content © Bungie, Inc. All rights reserved. Destiny, the Destiny
+        Logo, Bungie and the Bungie logo are among the trademarks of Bungie,
+        Inc.
+        <br />
+        Destiny Icons provided by{" "}
+        <a
+          href="https://github.com/justrealmilk/destiny-icons"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          justrealmilk/destiny-icons
+        </a>
+      </div>
+      <div className="md:flex-auto md:flex-row-reverse mt-2 flex-row flex">
+        <a href="https://twitter.com/kasn" className="w-12 mx-1">
+          Twitter
+        </a>
+        <a
+          href="https://github.com/kasn/d2toolbox"
+          className="w-12 mx-1"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+  </div>
+);
+
+export default Footer;
