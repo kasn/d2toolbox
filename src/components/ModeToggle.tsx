1057c0a0e89539afcf499da9e1f40bb7075dad8d
Karsten Gohm <karsten.gohm@internations.org>
2023-02-18 18:07:31 +0100

    typez

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -1,4 +1,6 @@
-function SunIcon(props) {
+import { SVGProps } from "react";
+
+function SunIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) {
   return (
     <svg viewBox="0 0 20 20" fill="none" aria-hidden="true" {...props}>
       <path d="M12.5 10a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Z" />
@@ -10,7 +12,7 @@ function SunIcon(props) {
   );
 }
 
-function MoonIcon(props) {
+function MoonIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) {
   return (
     <svg viewBox="0 0 20 20" fill="none" aria-hidden="true" {...props}>
       <path d="M15.224 11.724a5.5 5.5 0 0 1-6.949-6.949 5.5 5.5 0 1 0 6.949 6.949Z" />
