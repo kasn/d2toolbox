72df92fd94dda778b72eb31567fb8862803b1759
Karsten Gohm <karsten.gohm@internations.org>
2025-01-03 14:48:41 +0100

    do not redefined methods on each render

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -20,28 +20,28 @@ function MoonIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) {
   );
 }
 
+function disableTransitionsTemporarily() {
+  document.documentElement.classList.add("[&_*]:!transition-none");
+  window.setTimeout(() => {
+    document.documentElement.classList.remove("[&_*]:!transition-none");
+  }, 0);
+}
+
+function toggleMode() {
+  disableTransitionsTemporarily();
+
+  let darkModeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+  let isSystemDarkMode = darkModeMediaQuery.matches;
+  let isDarkMode = document.documentElement.classList.toggle("dark");
+
+  if (isDarkMode === isSystemDarkMode) {
+    delete window.localStorage.isDarkMode;
+  } else {
+    window.localStorage.isDarkMode = isDarkMode;
+  }
+}
+
 export function ModeToggle() {
-  function disableTransitionsTemporarily() {
-    document.documentElement.classList.add("[&_*]:!transition-none");
-    window.setTimeout(() => {
-      document.documentElement.classList.remove("[&_*]:!transition-none");
-    }, 0);
-  }
-
-  function toggleMode() {
-    disableTransitionsTemporarily();
-
-    let darkModeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
-    let isSystemDarkMode = darkModeMediaQuery.matches;
-    let isDarkMode = document.documentElement.classList.toggle("dark");
-
-    if (isDarkMode === isSystemDarkMode) {
-      delete window.localStorage.isDarkMode;
-    } else {
-      window.localStorage.isDarkMode = isDarkMode;
-    }
-  }
-
   return (
     <button
       type="button"
