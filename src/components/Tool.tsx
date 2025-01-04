ec2c82ef73190ed52b59f8994835819929b7b9c8
Karsten Gohm <karsten.gohm@internations.org>
2025-01-04 11:48:06 +0100

    fuddeling

--- src/components/Tool.tsx
+++ src/components/Tool.tsx
@@ -1,30 +1,61 @@
-import Image from "next/image";
+import Image, { getImageProps } from "next/image";
 import { SocialIcon } from "react-social-icons";
+
+function getBackgroundImage(srcSet = "") {
+  const imageSet = srcSet
+    .split(", ")
+    .map((str) => {
+      const [url, dpi] = str.split(" ");
+      return `url("${url}") ${dpi}`;
+    })
+    .join(", ");
+  return `image-set(${imageSet})`;
+}
 
 type ToolProps = {
   tool: TTool;
 };
 
 function Tool({ tool }: ToolProps) {
+  // const {
+  //   props: { src },
+  // } = getImageProps({
+  //   alt: "",
+  //   width: tool.image.width,
+  //   height: tool.image.height,
+  //   src: tool.image.src,
+  // });
+
+  // console.log(src);
+
+  // const backgroundImage = getBackgroundImage(srcSet);
+  // console.log(backgroundImage);
+  // const style = { backgroundImage };
+  // const c = `aspect-[5/4] w-full rounded-xl object-cover shadow-lg bg-[url(${src})] bg-cover bg-center`;
+  // console.log(c);
   return (
     <li>
-      <h2 className="mb-3 text-3xl font-bold leading-8 tracking-tight text-gray-900 dark:text-white">
-        <a href={tool.url} target="_blank" rel="noreferrer">
-          {tool.name}
-        </a>
-      </h2>
-      <a href={tool.url} target="_blank" rel="noreferrer">
-        <Image
+      <a
+        href={tool.url}
+        target="_blank"
+        rel="noreferrer"
+        className="aspect-[5/4] w-full rounded-xl object-cover shadow-lg bg-cover bg-center"
+        // className={`aspect-[5/4] w-full rounded-xl object-cover shadow-lg bg-[url(${src})] bg-cover bg-center`}
+      >
+        {/* <Image
           alt={tool.name}
-          className="aspect-[5/4] w-full rounded-xl object-cover shadow-lg"
           src={tool.image.src}
           width={tool.image.width}
           height={tool.image.height}
-        />
+        /> */}
+        <h2 className="mb-3 text-3xl font-bold leading-8 tracking-tight text-gray-900 dark:text-white">
+          {tool.name}
+        </h2>
+        <p className="mt-3 text-base leading-7 text-gray-600 dark:text-white">
+          {tool.description}
+        </p>
       </a>
-      <p className="mt-3 text-base leading-7 text-gray-600 dark:text-white">
-        {tool.description}
-      </p>
+
       {tool.additionalLinks && (
         <ul role="list" className="mt-3 flex gap-x-3">
           {tool.additionalLinks.map((link) => (
