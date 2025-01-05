415c3826458d2ff9a553829c6f89afaeec9600e6
Karsten Gohm <karsten.gohm@internations.org>
2025-01-05 13:28:46 +0100

    restructure

--- src/components/Tool.tsx
+++ src/components/Tool.tsx
@@ -17,44 +17,35 @@ type ToolProps = {
 };
 
 function Tool({ tool }: ToolProps) {
-  // const {
-  //   props: { src },
-  // } = getImageProps({
-  //   alt: "",
-  //   width: tool.image.width,
-  //   height: tool.image.height,
-  //   src: tool.image.src,
-  // });
+  const {
+    props: { src },
+  } = getImageProps({
+    alt: "",
+    width: tool.image.width,
+    height: tool.image.height,
+    src: tool.image.src,
+  });
 
-  // console.log(src);
-
-  // const backgroundImage = getBackgroundImage(srcSet);
-  // console.log(backgroundImage);
-  // const style = { backgroundImage };
-  // const c = `aspect-[5/4] w-full rounded-xl object-cover shadow-lg bg-[url(${src})] bg-cover bg-center`;
-  // console.log(c);
+  // const backgroundImage = getBackgroundImage(src);
+  console.log(src);
+  const style = {
+    backgroundImage: `url(${src})`,
+  };
   return (
     <li>
+      <a href={tool.url} target="_blank" rel="noreferrer"></a>
       <a
         href={tool.url}
         target="_blank"
         rel="noreferrer"
-        className="aspect-[5/4] w-full rounded-xl object-cover shadow-lg bg-cover bg-center"
-        // className={`aspect-[5/4] w-full rounded-xl object-cover shadow-lg bg-[url(${src})] bg-cover bg-center`}
+        className="aspect-[5/4] w-full rounded-xl object-cover shadow-lg bg-cover block relative"
+        style={style}
       >
-        {/* <Image
-          alt={tool.name}
-          src={tool.image.src}
-          width={tool.image.width}
-          height={tool.image.height}
-        /> */}
-        <h2 className="mb-3 text-3xl font-bold leading-8 tracking-tight text-gray-900 dark:text-white">
+        <h2 className="text-3xl font-bold leading-8 tracking-tight text-white p-6 top-0 left-0 right-0 absolute bg-gradient-to-t from-transparent to-zinc-900 rounded-xl">
           {tool.name}
         </h2>
-        <p className="mt-3 text-base leading-7 text-gray-600 dark:text-white">
-          {tool.description}
-        </p>
       </a>
+      <p className="mt-3  dark:text-white">{tool.description}</p>
 
       {tool.additionalLinks && (
         <ul role="list" className="mt-3 flex gap-x-3">
