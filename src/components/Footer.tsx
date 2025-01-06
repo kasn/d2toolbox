f212d923d06b6468fbc098e3947ea0b1fe28f29c
Karsten Gohm <karsten.gohm@internations.org>
2025-01-06 11:46:07 +0100

    main content should only be sites, description is meh

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,19 @@ import { SocialIcon } from "react-social-icons";
 
 const Footer = () => (
   <div className="mt-5 bg-gray-100 pt-2 dark:bg-white/5">
+    <div className="m-auto max-w-6xl px-3 pt-5 text-sm text-gray-800 dark:text-white">
+      <p className="whitespace-normal text-xl text-gray-600 dark:text-white">
+        A list of valuable resources for every Guardian.
+      </p>
+      <p className="textwhitespace-normal text-gray-600 dark:text-white">
+        Destiny 2 is an enourmous game that can be overwhelming for new players.
+        <br />
+        We aim to create an exhaustive list of all the tools that will help to
+        make the most out of the game.
+      </p>
+    </div>
     <div className="m-auto flex max-w-6xl flex-col px-3 pb-5 pt-5 text-sm text-gray-800 dark:text-white md:flex-row">
-      <div className="mt-2">
+      <div>
         © Copyright {new Date().getFullYear()}. All Rights Reserved.
         <br />
         Bungie Content © Bungie, Inc. All rights reserved. Destiny, the Destiny
