e94f33f71ecb8862dd1a5162df775b3ef01d5503
Karsten Gohm <kasn@mac.com>
2025-02-08 12:09:04 +0100

    typo

--- src/pages/sundering-doctrine.tsx
+++ src/pages/sundering-doctrine.tsx
@@ -6,10 +6,10 @@ export default function Home() {
         <h2 className="m-3 dark:text-white text-xl">symbols</h2>
         <img src="/images/sd/vow-of-the-disciple-symbols.jpg" alt="" width={1000}/>        
         
-        <h2 className="m-3 dark:text-white text-xl">encouter 1</h2>
+        <h2 className="m-3 dark:text-white text-xl">encounter 1</h2>
         <img src="/images/sd/IMG_5098.PNG" alt="" width={1000}/>        
-        <h2 className="m-3 dark:text-white text-xl">encouter 2</h2>
-        <h2 className="m-3 dark:text-white text-xl">encouter 3</h2>
+        <h2 className="m-3 dark:text-white text-xl">encounter 2</h2>
+        <h2 className="m-3 dark:text-white text-xl">encounter 3</h2>
         <img src="/images/sd/IMG_5097.PNG" alt="" width={1000}/>
     </>
 }
