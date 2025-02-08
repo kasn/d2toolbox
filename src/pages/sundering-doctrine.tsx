ee3c462a0fcc35069742030c38997592410e0601
Karsten Gohm <kasn@mac.com>
2025-02-08 11:55:19 +0100

    sundering doctrine page

---
+++ src/pages/sundering-doctrine.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image"
+
+export default function Home() {
+    return <>
+        <h1>Sundering Doctrine</h1>
+        <h2>symbols</h2>
+        <img src="/images/sd/vow-of-the-disciple-symbols.jpg" alt="" width={1000}/>        
+        
+        <h2>encouter 1</h2>
+        <img src="/images/sd/IMG_5098.PNG" alt="" width={1000}/>        
+        <h2>encouter 2</h2>
+        <h2>encouter 3</h2>
+        <img src="/images/sd/IMG_5097.PNG" alt="" width={1000}/>
+    </>
+}
