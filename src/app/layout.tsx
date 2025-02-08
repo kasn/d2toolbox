28baf6e6107d6fc448aa4a7c5df50f2535fc59e5
Karsten Gohm <kasn@mac.com>
2025-02-08 19:55:53 +0100

    app router (#26)
    yay app router

---
+++ src/app/layout.tsx
@@ -0,0 +1,54 @@
+import type { Metadata, Viewport } from "next";
+import { Analytics } from "@vercel/analytics/react";
+import { ModeToggle } from "@/components/ModeToggle";
+import DestinyIcon from "@/components/DestinyIcon";
+import Footer from "@/components/Footer";
+import { Providers } from "./providers";
+
+import "../styles/globals.css";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "Destiny 2 Toolbox",
+  description: "everything every guardians needs every day",
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <link rel="icon" href="/favicon.ico" />
+      <head />
+      <body className="bg-white antialiased dark:bg-zinc-900">
+        <Providers>
+          <main>
+            <header className="dark:border-b-white/7.5 backdrop-blur-xs fixed inset-x-0 top-0 z-50 flex h-14 items-center justify-between gap-12 border-b px-4 sm:px-6 lg:z-30 lg:px-8">
+              <div className="items-left flex gap-5">
+                <Link href="/">
+                  <DestinyIcon className="h-7 w-7 fill-black dark:fill-white" />
+                </Link>
+                <h1 className="pl-4 text-2xl font-bold dark:text-white">
+                  <Link href="/">Destiny 2 Tools</Link>
+                </h1>
+              </div>
+              <div className="items-right flex gap-5">
+                <ModeToggle />
+              </div>
+            </header>
+            {children}
+            <Footer />
+          </main>
+        </Providers>
+        <Analytics />
+      </body>
+    </html>
+  );
+}
---
+++ src/app/sundering-doctrine/page.tsx
@@ -0,0 +1,19 @@
+export default function SunderingDoctrine() {
+  return (
+    <article className="not-focus-visible:focus:outline-hidden m-14 mt-20">
+      <h1 className="m-3 text-xl dark:text-white">Sundering Doctrine</h1>
+      <h2 className="m-3 text-xl dark:text-white">symbols</h2>
+      <img
+        src="/images/sd/vow-of-the-disciple-symbols.jpg"
+        alt=""
+        width={1000}
+      />
+
+      <h2 className="m-3 text-xl dark:text-white">encounter 1</h2>
+      <img src="/images/sd/IMG_5098.PNG" alt="" width={1000} />
+      <h2 className="m-3 text-xl dark:text-white">encounter 2</h2>
+      <h2 className="m-3 text-xl dark:text-white">encounter 3</h2>
+      <img src="/images/sd/IMG_5097.PNG" alt="" width={1000} />
+    </article>
+  );
+}
