97aaeb24de773e96479c8a959e240a62d1eb1cc5
Karsten Gohm <kasn@mac.com>
2025-02-08 12:06:28 +0100

    fix text on dark

--- src/pages/sundering-doctrine.tsx
+++ src/pages/sundering-doctrine.tsx
@@ -2,14 +2,14 @@ import Image from "next/image"
 
 export default function Home() {
     return <>
-        <h1>Sundering Doctrine</h1>
-        <h2>symbols</h2>
+        <h1 className="m-3 dark:text-white text-xl">Sundering Doctrine</h1>
+        <h2 className="m-3 dark:text-white text-xl">symbols</h2>
         <img src="/images/sd/vow-of-the-disciple-symbols.jpg" alt="" width={1000}/>        
         
-        <h2>encouter 1</h2>
+        <h2 className="m-3 dark:text-white text-xl">encouter 1</h2>
         <img src="/images/sd/IMG_5098.PNG" alt="" width={1000}/>        
-        <h2>encouter 2</h2>
-        <h2>encouter 3</h2>
+        <h2 className="m-3 dark:text-white text-xl">encouter 2</h2>
+        <h2 className="m-3 dark:text-white text-xl">encouter 3</h2>
         <img src="/images/sd/IMG_5097.PNG" alt="" width={1000}/>
     </>
 }
