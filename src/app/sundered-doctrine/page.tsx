87afdac876edad04b09da9874a48195174b74369
Karsten Gohm <karsten.gohm@internations.org>
2025-02-15 15:04:49 +0100

    add accordion to sundered-doctrine page for better visibility

--- src/app/sundered-doctrine/page.tsx
+++ src/app/sundered-doctrine/page.tsx
@@ -1,3 +1,10 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
 export default function SunderingDoctrine() {
   return (
     <article className="not-focus-visible:focus:outline-hidden m-14 mt-20">
@@ -8,12 +15,24 @@ export default function SunderingDoctrine() {
         alt=""
         width={1000}
       />
-
-      <h2 className="m-3 text-xl dark:text-white">encounter 1</h2>
-      <img src="/images/sd/IMG_5098.PNG" alt="" width={1000} />
-      <h2 className="m-3 text-xl dark:text-white">encounter 2</h2>
-      <h2 className="m-3 text-xl dark:text-white">encounter 3</h2>
-      <img src="/images/sd/IMG_5097.PNG" alt="" width={1000} />
+      <Accordion type="single" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>
+            <h2 className="m-3 text-xl dark:text-white">encounter 1</h2>
+          </AccordionTrigger>
+          <AccordionContent>
+            <img src="/images/sd/IMG_5098.PNG" alt="" width={1000} />
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-2">
+          <AccordionTrigger>
+            <h2 className="m-3 text-xl dark:text-white">encounter 3</h2>
+          </AccordionTrigger>
+          <AccordionContent>
+            <img src="/images/sd/IMG_5097.PNG" alt="" width={1000} />
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </article>
   );
 }
