32f0eb452d1a6d1033eea0dd1525b202b80356ec
Karsten Gohm <kasn@mac.com>
2025-10-19 11:18:06 +0200

    add nitro

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   server: {
@@ -19,34 +20,8 @@ export default defineConfig({
         // Specifies the directory TanStack Router uses for your routes.
         routesDirectory: "app", // Defaults to "routes", relative to srcDirectory
       },
-      prerender: {
-        // Enable prerendering
-        enabled: true,
-
-        // Enable if you need pages to be at `/page/index.html` instead of `/page.html`
-        autoSubfolderIndex: true,
-
-        // How many prerender jobs to run at once
-        concurrency: 14,
-
-        // Whether to extract links from the HTML and prerender them also
-        crawlLinks: true,
-
-        // Filter function takes the page object and returns whether it should prerender
-        filter: ({ path }) => !path.startsWith("/do-not-render-me"),
-
-        // Number of times to retry a failed prerender job
-        retryCount: 2,
-
-        // Delay between retries in milliseconds
-        retryDelay: 1000,
-
-        // Callback when page is successfully rendered
-        onSuccess: ({ page }) => {
-          console.log(`Rendered ${page.path}!`);
-        },
-      },
     }),
     viteReact(),
+    nitro(),
   ],
 });
